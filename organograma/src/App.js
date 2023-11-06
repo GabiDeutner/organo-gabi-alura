9ec280b967b54779da3bfe4bdac7cab425fb9f5d
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-11-06 20:07:17 -0300

    Criando o formulário

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o enderço da imagem"/>
+      <Formulario/>
     </div>
   );
 }
---
+++ organograma/src/componentes/Formulario/index.js
@@ -0,0 +1,17 @@
+import CampoTexto from './componentes/CampoTexto';
+import './Formulario.css'
+
+const Formulario = () => {
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o enderço da imagem"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario;
