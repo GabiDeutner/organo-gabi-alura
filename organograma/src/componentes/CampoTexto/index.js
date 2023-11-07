ad3b6b3ff805617f3a07bb17ac13bf28eebb17a1
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-07 17:38:15 -0300

    Manipulando o input do usuário

--- organograma/src/componentes/CampoTexto/index.js
+++ organograma/src/componentes/CampoTexto/index.js
@@ -4,12 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` // exemplo interpolar strings com props
 
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.targed.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/> 
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/> 
         </div>
     )
 }
