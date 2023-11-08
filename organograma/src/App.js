7f22b79d8f72fabe9144ee7ec603c745c5bed87f
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-11-08 12:03:18 -0300

    Refatorando código duplicado

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -54,7 +54,7 @@ function App() {
   return (
     <div className="App">
       <Banner/>
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
       
       {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corprimaria} corSecundaria={time.corSecundaria}/>)}
       
--- organograma/src/componentes/Formulario/index.js
+++ organograma/src/componentes/Formulario/index.js
@@ -3,16 +3,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = (
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    )
-
     const [nome,setNome] = useState('')
     const [cargo,setCargo] = useState('')
     const [imagem,setImagem] = useState('')
@@ -57,7 +47,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
