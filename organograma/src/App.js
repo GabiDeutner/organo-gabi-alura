3789e91b09b2606709345fb863976a622b96d36c
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-11-08 11:15:14 -0300

    Criando o componente Time

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
