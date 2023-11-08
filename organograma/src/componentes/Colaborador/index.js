bb72ffc44e0ab39907095ad8aa686d03d88d9c30
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-11-08 13:17:34 -0300

    Card de colaborador

---
+++ organograma/src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/GabiDeutner.png' alt='Gabi Deutner' />
+        </div>
+        <div className='rodape'>
+            <h4>Gabi Deutner</h4>
+            <h5>Desenvolvedora</h5>
+        </div>
+    </div>
+
+    )
+}
+
+export default Colaborador
+
--- organograma/src/componentes/Time/index.js
+++ organograma/src/componentes/Time/index.js
@@ -6,6 +6,8 @@ const Time = (props) => {
     return (
         <section className='time' style={css}>
             <h3 style = {{borderColor: props.corprimaria}}>{props.nome}</h3>
+            <Colaborador/>
+            <Colaborador/>
         </section>
     )
 }
