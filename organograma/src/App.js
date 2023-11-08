d7a7376053bcbe3a6817d5de98c4b3aa8bbce2aa
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-11-08 11:40:14 -0300

    Elevando a lista de times

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -6,6 +6,44 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -17,9 +55,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
-      <Time nome="Programação"/>
-      <Time nome="Frontend"/>
-      <Time nome="Data Science"/>
+      
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corprimaria} corSecundaria={time.corSecundaria}/>)}
+      
     </div>
   );
 }
